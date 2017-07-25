45fb7580e0f62e1a95d0ae232d3464def7a14076
Lmilne <lmilne360@gmail.com>
2017-07-24 23:59:46 -0400

    Updated featured section

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedComponent implements OnInit {
 
+	cards = [
+	{category: "Some Category", title: "Some title", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus sed dolor varius aliquet in ut nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque purus leo, aliquam a eleifend quis, convallis in diam. Sed eu velit a odio rhoncus viverra in vitae quam. Maecenas eu lorem nisl." },
+	{category: "Some Category", title: "Some title", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus sed dolor varius aliquet in ut nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque purus leo, aliquam a eleifend quis, convallis in diam. Sed eu velit a odio rhoncus viverra in vitae quam. Maecenas eu lorem nisl." },
+	{category: "Some Category", title: "Some title", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus sed dolor varius aliquet in ut nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque purus leo, aliquam a eleifend quis, convallis in diam. Sed eu velit a odio rhoncus viverra in vitae quam. Maecenas eu lorem nisl." }
+	]
+
   constructor() { }
 
   ngOnInit() {
